61a666b5ede609716b3a4d43853188849410a700
tefifi <s.mercado01@ufromail.cl>
2025-07-08 22:29:24 -0400

    Delete Ratatui2.0 directory

--- Ratatui2.0/src/main/java/Diabetes.java
+++
@@ -1,8 +0,0 @@
-public class Diabetes extends Condicion{
-    private int maxAzucar;
-
-    public int obtenerValor(){
-        this.maxAzucar = 10;
-        return maxAzucar;
-    }
-}
--- Ratatui2.0/src/main/java/Hipertension.java
+++
@@ -1,8 +0,0 @@
-public class Hipertension {
-    private int maxSodio;
-
-    public int obtenerValor(){
-        this.maxSodio = 110;
-        return maxSodio;
-    }
-}
